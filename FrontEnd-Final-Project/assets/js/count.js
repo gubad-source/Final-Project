ad98b44c57cd284fd9bfacb5e80346a0fffae468
gubad-source <qubadmustafayev@gmail.com>
2020-06-08 16:14:16 +0400

    index page has been changed

---
+++ FrontEnd-Final-Project/assets/js/count.js
@@ -0,0 +1,4 @@
+$(".count").counterUp({
+    delay:100,
+    time:1000
+})
--- FrontEnd-Final-Project/assets/js/main.js
+++ FrontEnd-Final-Project/assets/js/main.js
@@ -14,4 +14,19 @@ $(document).ready(function () {
     $(window).on('load', function () {
         wowAnimation();
     });
+
+
+
+    if ($('#third-section .tab-grid').length) {
+        var $grid = $('#courses .courses').isotope({
+            itemSelector: '.grid-item',
+            percentPosition: true,
+        });
+
+        $('#tabs li').on('click', 'button', function () {
+            
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+    }
 });
